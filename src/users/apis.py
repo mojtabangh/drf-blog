6c2c06002c7d702442a744343b6b41199fd2038f
mojtabangh <mojtaba83ngh@gmail.com>
2025-02-18 19:11:28 +0330

    feat(users): write register api

---
+++ src/users/apis.py
@@ -0,0 +1,57 @@
+from django.core.validators import MinLengthValidator, MaxLengthValidator
+from rest_framework.views import APIView
+from rest_framework import serializers
+from rest_framework import status
+from rest_framework.response import Response
+
+from .models import User
+from .services import create_user
+
+
+class UserRegisterApi(APIView):
+    class InputUserRegisterSerializer(serializers.Serializer):
+        email = serializers.EmailField(required=True,)
+        password = serializers.CharField(
+            required=True,
+            validators=[
+                MinLengthValidator(limit_value=10),
+                MaxLengthValidator(limit_value=32),
+            ],
+        )
+        password = serializers.CharField(required=True,)
+
+        def validate(self, data):
+            if not data.get("password") or not data.get("confirm_password"):
+                raise serializers.ValidationError("You must enter password and confirm password")
+
+            if data.get("password") != data.get("confirm_password"):
+                raise serializers.ValidationError("Password and confirm password are not the same.")
+
+            return data
+
+    class OutputUserRegisterSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = User
+            fields = ("email", "created_at", "updated_at")
+
+    def post(self, request):
+        data = self.InputUserRegisterSerializer(data=request.data)
+        data.is_valid(raise_exception=True)
+        try:
+            user = create_user(
+                email=data.validated_data("email"),
+                password=data.validated_data("password"),
+                is_active=True
+            )
+            return user
+
+        except Exception as ex:
+            return Response(
+                f"{ex}",
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        return Response(
+            self.OutputUserRegisterSerializer(user),
+            status=status.HTTP_201_CREATED
+        )
