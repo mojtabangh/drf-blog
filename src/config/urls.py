13c703447af2702fe5d15eaf031695fc307742a9
mojtabangh <mojtaba83ngh@gmail.com>
2025-03-01 21:33:14 +0330

    add docs

--- src/config/urls.py
+++ src/config/urls.py
@@ -4,7 +4,16 @@ URL configuration for config project.
 from django.contrib import admin
 from django.urls import path, include
 
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
+
 urlpatterns = [
+    path("schema/", SpectacularAPIView.as_view(api_version="v1"), name="schema"),
+    path("", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path("redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
