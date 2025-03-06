8befda4cf3bee2e5aad0214d4dde9bf50e0641bc
mojtabangh <mojtaba83ngh@gmail.com>
2025-03-07 00:23:50 +0330

    add paginator with context

--- src/api/pagination.py
+++ src/api/pagination.py
@@ -14,6 +14,15 @@ def get_paginated_response(*, pagination_class, serializer_class, queryset, requ
     return paginator.get_paginated_response(serializer.data)
 
 
+def get_paginated_response_context(*, pagination_class, serializer_class, queryset, request, view):
+    paginator = pagination_class()
+
+    page = paginator.paginate_queryset(queryset, request, view=view)
+
+    serializer = serializer_class(page, many=True, context={'request': request})
+    return paginator.get_paginated_response(serializer.data)
+
+
 class LimitOffsetPagination(_LimitOffsetPagination):
     default_limit = 10
     max_limit = 50
